b4a4b83564fa4bf6352cdd3f638bd3aba03f30ed
mutac <mutac@github>
2013-01-09 12:07:31 -0800

    initial commit

---
+++ AnalogReadTest/AnalogReadTest.ino
@@ -0,0 +1,17 @@
+
+int LightSensorIn = A0; // analog
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int lightSensorValue = analogRead(LightSensorIn);
+  float voltage = lightSensorValue * (5.0 / 1023.0);
+  
+  Serial.println(voltage);
+  
+  delay(500);
+}
