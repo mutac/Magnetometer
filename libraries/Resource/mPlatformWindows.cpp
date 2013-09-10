ceb5ccf296262d1341e60617be34841f41ee2682
mutac <mutac@github>
2013-09-09 22:52:33 -0700

    Added atomic operations for int type on windows

--- libraries/Resource/mPlatformWindows.cpp
+++ libraries/Resource/mPlatformWindows.cpp
@@ -1,32 +1,39 @@
 
 #include "mPlatform.h"
+#include <windows.h>
 
 #ifdef mPlatformWindows
 
 namespace mPlatform
 {
+  mCompilerAssert(LongIntegerTypeSizesMatch, sizeof(LONG) == sizeof(int));
+
   template <>
   int atomic_fetch_and_add(int volatile* v, int dV)
   {
-    return 0;
+    mAssert((INT_PTR)v % 4 == 0);
+    return InterlockedExchangeAdd((volatile LONG*)v, dV);
   }
 
   template <>
   int atomic_fetch_and_subtract(int volatile* v, int dV)
   {
-    return 0;
+    mAssert((INT_PTR)v % 4 == 0);
+    return InterlockedExchangeAdd((volatile LONG*)v, -dV);
   }
 
   template <>
   int atomic_add_and_fetch(int volatile* v, int dV)
   {
-    return 0;
+    mAssert((INT_PTR)v % 4 == 0);
+    return InterlockedExchangeAdd((volatile LONG*)v, dV) + dV;
   }
 
   template <>
   int atomic_subract_and_fetch(int volatile* v, int dV)
   {
-    return 0;
+    mAssert((INT_PTR)v % 4 == 0);
+    return InterlockedExchangeAdd((volatile LONG*)v, -dV) - dV;
   }
 }
 
